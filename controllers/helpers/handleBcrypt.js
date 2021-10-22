65627d308d79fbd6b49823d8584991d0cd050080
Alexis Beltrán <jesus_beltran011@outlook.com>
2021-10-21 22:19:12 -0600

    Login semi-complete

--- controllers/helpers/handleBcrypt.js
+++ controllers/helpers/handleBcrypt.js
@@ -4,7 +4,15 @@ const encrypt = async (Text) => {
     const hash = await bcryptjs.hash(Text, 8);
     return hash;
 }
-
+const compare = async (passwordPlain, passowrdHash) =>{
+    try{
+        return await bcryptjs.compare(passwordPlain, passowrdHash);
+    }catch(Error){
+        console.log(Error);
+    }
+    
+}
 export{
-    encrypt
+    encrypt,
+    compare
 }
