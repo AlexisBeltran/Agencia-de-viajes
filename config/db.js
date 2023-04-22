f019693f4a3b1c6b00c5f4146f3c2b91be0a3999
AlexisBeltran <jesus_beltran011@outlook.com>
2023-04-22 00:52:51 -0600

    Update env

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import Sequelize from "sequelize";
 import dotenv from 'dotenv';
 
-dotenv.config({path: 'variables.env'});
+dotenv.config({path: '.env'});
 const {BD_NOMBRE, BD_USER, BD_PASS, BD_HOST, BD_PORT} = process.env;
 
 const db = new Sequelize(BD_NOMBRE, BD_USER, BD_PASS,{
